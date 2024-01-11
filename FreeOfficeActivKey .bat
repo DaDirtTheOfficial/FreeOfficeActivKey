a87aa59733dc359347bcd491e053399e968f0f04
Əñźø <116453766+DaDirtTheOfficial@users.noreply.github.com>
2024-01-11 18:00:51 +0200

    Create FreeOfficeActivKey .bat

---
+++ FreeOfficeActivKey .bat
@@ -0,0 +1,17 @@
+@echo off
+title Activate Microsoft Office 2021 (ALL versions) for FREE  - &cls&echo =====================================================================================&echo Project: Activating Microsoft software products for FREE without additional software made by Enzo &echo =====================================================================================&echo.&echo #Supported products:&echo - Microsoft Office Standard 2021&echo - Microsoft Office Professional Plus 2021&echo.&echo.&(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")&(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")&(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2021VL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)&echo.&echo =====================================================================================&echo Activating your product...&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo ospp.vbs /setprt:1688 >nul&cscript //nologo ospp.vbs /unpkey:6F7TH >nul&set i=1&cscript //nologo ospp.vbs /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH >nul||goto notsupported
+:skms
+if %i% GTR 10 goto busy
+if %i% EQU 1 set KMS=kms7.MSGuides.com
+if %i% EQU 2 set KMS=e8.us.to
+if %i% EQU 3 set KMS=e9.us.to
+if %i% GTR 3 goto ato
+cscript //nologo ospp.vbs /sethst:%KMS% >nul
+:ato
+echo =====================================================================================&echo.&echo.&cscript //nologo ospp.vbs /act | find /i "successful" && (echo.&echo =====================================================================================&echo.&echo #My official blog:#Please feel free to contact me at dadirttheofficial@gmail.com if you have any questions or concerns.&echo.&echo #Please consider supporting this project: https://github.com/dadirttheofficial #Your support is helping me so much!&echo.&echo =====================================================================================
+:notsupported
+echo =====================================================================================&echo.&echo Sorry, your version is not supported.&echo.&goto halt
+:busy
+echo =====================================================================================&echo.&echo Sorry, the server is busy and can't respond to your request. Please try again.&echo.
+:halt
+pause >nul
